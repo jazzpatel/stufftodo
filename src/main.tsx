61c12ff1c1361e0915190b538c129a8897190d10
Jazz <jazz_patel@yahoo.com>
2026-08-15 11:00:42 -0400

    updated vite config

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
+console.log("App starting...");
+console.log(import.meta.env);
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import commonjs from "vite-plugin-commonjs";
+import dotenv from "dotenv";
+dotenv.config();
 
 export default defineConfig({
   build: {
     outDir: "dist",
     emptyOutDir: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
   },
-  plugins: [react()],
+  define: {},
+  plugins: [react(), commonjs()],
   server: {
     host: true, // same as --host, binds to 0.0.0.0
     port: 5183, // optional, default is 5173
